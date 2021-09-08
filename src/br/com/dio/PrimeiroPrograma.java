ad3c312c069f159d0954e89be77eae2de538375e
Marcelo Senna <marcelosennarj84@gmail.com>
2021-09-08 19:00:31 -0300

    criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,6 +8,8 @@ public class PrimeiroPrograma {
 
         System.out.println(gato);
 
+        Livro livro2 = new Livro("Os problemas dos Deuses", 300);
+        System.out.println(livro2);
 
         /*int a = 5;
         int b = 3;
@@ -15,3 +17,36 @@ public class PrimeiroPrograma {
     }
 }
 
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+}
